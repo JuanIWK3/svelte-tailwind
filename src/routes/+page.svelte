b034807ed0f5f0cc2afbcb803edd1972edeece4d
juaniwk3 <juaniwk3@gmail.com>
2023-05-03 12:53:08 -0300

    feat: dialog page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,54 +1,15 @@
-<div class="container-outer">
-	<div class="container-inner">
-		<div class="card z-10">
-			<div class="card-body">
-				<div class="card-title text-4xl font-bold">
-					<h1 class="title">Serviços</h1>
-				</div>
-				<ul>
-					<li>Manutenção</li>
-					<li>Instalação</li>
-					<li>Limpeza</li>
-					<li>Reparo</li>
-				</ul>
-			</div>
-		</div>
-	</div>
-	<div class="container-inner bg-base-200">
-		<div class="card">
-			<div class="card-body">
-				<div class="card-title text-4xl font-bold">
-					<h1 class="title">Produtos</h1>
-				</div>
-				<ul>
-					<li>Mousepad</li>
-					<li>Headset</li>
-					<li>Monitor</li>
-					<li>Teclado</li>
-					<li>Mouse</li>
-				</ul>
-			</div>
+<script lang="ts">
+</script>
+
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content text-center">
+		<div class="max-w-md">
+			<h1 class="text-5xl font-bold">Hello there</h1>
+			<p class="py-6">DO YOU KNOW DA WAE</p>
+			<button class="btn btn-primary">Nothing</button>
 		</div>
 	</div>
 </div>
 
 <style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.gray.100);
-	}
-
-	.container-outer {
-		@apply flex flex-col h-screen sm:flex-row;
-	}
-
-	.container-inner {
-		@apply flex flex-col items-center justify-center h-screen w-full cursor-pointer transition-all text-center;
-		@apply hover:h-[110%] sm:hover:w-[110%] sm:hover:h-screen;
-	}
-	.card {
-		@apply transition-all duration-200;
-	}
-	.container-inner:hover .card {
-		@apply scale-105;
-	}
 </style>
---
+++ src/routes/dialog/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let dialog: HTMLDialogElement;
+	onMount(() => {
+		dialog = document.getElementById('dialog') as HTMLDialogElement;
+	});
+
+	const showDialog = () => {
+		dialog.showModal();
+	};
+</script>
+
+<div class="flex items-center justify-center min-h-screen">
+	<dialog id="dialog" class="card hidden">
+		<div class="card-body">
+			<div class="card-title">Die Dialog</div>
+			<div class="card-text">
+				Die <abbr>Dialog</abbr> ist ein HTML-Element, das als Modal verwendet werden kann. Es ist ein
+				neues Element in HTML5.
+			</div>
+			<div class="card-actions justify-end">
+				<button on:click={() => dialog.close()} class="btn btn-primary"> schließen </button>
+			</div>
+		</div>
+	</dialog>
+	<button on:click={showDialog} class="btn">Zeigen</button>
+</div>
+
+<style lang="postcss">
+	#dialog[open] {
+		@apply flex;
+	}
+</style>
